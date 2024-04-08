3c7459fb88beb0aa5244063170a7198b1791e1a2
Ridwan Muhammad Raihan <65919906+ridwanmuh3@users.noreply.github.com>
2024-04-08 12:57:40 +0700

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ const directToQuizPage = () => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			try {
-				window.location.href += `quiz.html`
+				window.location.href = `${window.location.origin}/diabetes-melitus-quiz-app/quiz.html`
 
 				resolve()
 			} catch (error) {
